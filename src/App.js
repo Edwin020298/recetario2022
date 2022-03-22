01110f08b00473306a24a78dfdbf49f8b7486ceb
Edwin020298 <edwinmaypoot@gmail.com>
2022-03-22 07:55:47 -0600

    agregue formularios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Container,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardBody,
+} from "reactstrap";
 
 function App() {
+  const [values, setvalues] = React.useState({
+    nombre: "",
+    calorias: "",
+    descripcion: "",
+    ingredientes: "",
+  });
+
+  function handleSubmit(evt) {
+    evt.preventDefauld();
+  }
+
+  function handleChange(evt) {
+    const { target } = evt;
+    const { name, value } = target;
+
+    const newValues = {
+      ...values,
+      [name]: value,
+    };
+    setvalues(newValues);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <img src={logo} className="App-logo" alt="logo" />
+          EL PRIMER RECETARIO DE EDWIN MAY
+          <img src={logo} className="App-logo" alt="logo" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <Container className="p-10">
+        <Card>
+          <CardBody>
+            <Form onSubmit={handleSubmit}>
+              <FormGroup>
+                <Label for="name">Nombre</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  value={values.Nombre}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="Calorias">Calorias</Label>
+                <Input
+                  type="text"
+                  name="Calorias"
+                  value={values.Calorias}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="Descripcion">Descripcion</Label>
+                <Input
+                  type="text"
+                  name="Descripcion"
+                  values={values.descripcion}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="ingredientes">ingredientes</Label>
+                <Input
+                  type="text"
+                  name="ingredientes"
+                  values={values.ingredientes}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+
+              <Button type="submit">Guardar receta</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Container>
     </div>
   );
 }
